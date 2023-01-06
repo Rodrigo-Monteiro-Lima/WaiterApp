3cc0617b98c271fbbebdeb4c7eaf8e53e767a12a
Rodrigo Monteiro Lima <rodrigoml2395@gmail.com>
2023-01-06 16:08:28 -0300

    style: header

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -1,15 +1,18 @@
 import logo from '../../assets/images/logo.svg';
+import { Container, Content } from './styles';
 
 
 const Header = () => {
   return (
-    <div>
-      <div className="page-details">
-        <h1>Pedidos</h1>
-        <h2>Acompanhe o pedido dos clientes</h2>
-      </div>
-      <img src={logo} alt="Logo Waiter App" />
-    </div>
+    <Container>
+      <Content>
+        <div className="page-details">
+          <h1>Pedidos</h1>
+          <h2>Acompanhe o pedido dos clientes</h2>
+        </div>
+        <img src={logo} alt="Logo Waiter App" />
+      </Content>
+    </Container>
   );
 };
 
