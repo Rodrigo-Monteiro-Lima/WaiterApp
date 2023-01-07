c7270974216209129c7c97215fa9c5e624cc8924
Rodrigo Monteiro Lima <rodrigoml2395@gmail.com>
2023-01-07 16:34:59 -0300

    feat: categories

--- mobile/src/components/Categories/index.tsx
+++ mobile/src/components/Categories/index.tsx
@@ -1,6 +1,38 @@
+import { categories } from '../../mocks/categories';
+import { Text } from '../Text';
+import { CategoryContainer, Icon } from './styles';
+import { FlatList } from 'react-native';
+import { useState } from 'react';
+
 const Categories = () => {
+  const [selectedCategory, setSelectedCategory] = useState('');
+  const handleSelectCategory = (categoryId: string) => {
+    const category = selectedCategory === categoryId ? '' : categoryId;
+    setSelectedCategory(category);
+  };
+
   return (
-    null
+    <FlatList
+      horizontal
+      contentContainerStyle={{ paddingRight: 24 }}
+      showsHorizontalScrollIndicator={false}
+      data={categories}
+      keyExtractor={category => category._id}
+      renderItem={({ item: category }) => {
+        const isSelected = selectedCategory === category._id;
+        return (
+          <CategoryContainer onPress={() => handleSelectCategory(category._id)}>
+            <Icon>
+              <Text opacity={isSelected ? 1 : 0.5}>{category.icon}</Text>
+            </Icon>
+
+            <Text opacity={isSelected ? 1 : 0.5} size={14} weight="600">
+              {category.name}
+            </Text>
+          </CategoryContainer>
+        );
+      }}
+    />
   );
 };
 
