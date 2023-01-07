a04b179ef15d9f277333abd9e875216fe5649394
Rodrigo Monteiro Lima <rodrigoml2395@gmail.com>
2023-01-07 15:32:17 -0300

    style: fonts

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,10 +1,18 @@
+import { useFonts } from 'expo-font';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [isFontLoaded] = useFonts({
+    'GeneralSans-400': require('./src/assets/fonts/GeneralSans-Regular.otf'),
+    'GeneralSans-600': require('./src/assets/fonts/GeneralSans-Semibold.otf'),
+    'GeneralSans-700': require('./src/assets/fonts/GeneralSans-Bold.otf'),
+  });
+
+  if (!isFontLoaded) return null;
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={{fontFamily: 'GeneralSans-400'}}>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
