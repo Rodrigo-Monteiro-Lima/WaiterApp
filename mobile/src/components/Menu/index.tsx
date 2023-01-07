aab22ae75581efbc37de3c9c4824db3247779d0a
Rodrigo Monteiro Lima <rodrigoml2395@gmail.com>
2023-01-07 16:55:25 -0300

    feat: menu

--- mobile/src/components/Menu/index.tsx
+++ mobile/src/components/Menu/index.tsx
@@ -1,6 +1,26 @@
+import { FlatList } from 'react-native';
+import { products } from '../../mocks/products';
+import { Text } from '../Text';
+
 const Menu = () => {
   return (
-    null
+    <FlatList
+      data={products}
+      style={{ marginTop: 32 }}
+      contentContainerStyle={{ paddingHorizontal: 24 }}
+      keyExtractor={(products) => products._id}
+      renderItem={({ item: product }) => (
+        <Product>
+          <Image>
+            <ProductDetails>
+              <Text>{product.name}</Text>
+              <Text>{product.description}</Text>
+              <Text>{product.price}</Text>
+            </ProductDetails>
+          </Image>
+        </Product>
+      )}
+    />
   );
 };
 
