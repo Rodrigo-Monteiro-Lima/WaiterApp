e5dab60da28c3446c61d72a6c392c1494534e784
Rodrigo Monteiro Lima <rodrigoml2395@gmail.com>
2023-01-08 20:20:52 -0300

    build: socket io

--- api/src/index.ts
+++ api/src/index.ts
@@ -2,21 +2,23 @@ import express from 'express';
 import path from 'node:path';
 import mongoose from 'mongoose';
 import { router } from './router';
+import http from 'node:http';
 
 mongoose.connect('mongodb://localhost:27017').then(() => {
-  const app = express();
   const port = 3001;
+  const app = express();
+  const server = http.createServer(app);
 
   app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Methods", "*");
-    res.setHeader("Access-Control-Allow-Headers", "*");
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', '*');
+    res.setHeader('Access-Control-Allow-Headers', '*');
     next();
   });
   app.use('/uploads', express.static(path.resolve(__dirname,  '..', 'uploads')));
   app.use(express.json());
   app.use(router);
-  app.listen(port, () => {
+  server.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
   });
 }).catch(() => console.log('erro ao conectar no mongodb'));
