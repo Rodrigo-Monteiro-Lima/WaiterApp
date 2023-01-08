dba96b625c8a1b168fdd24523ff90e26e5c1ca7c
Rodrigo Monteiro Lima <rodrigoml2395@gmail.com>
2023-01-08 20:38:48 -0300

    feat: connect socker

--- frontend/src/components/Orders/index.tsx
+++ frontend/src/components/Orders/index.tsx
@@ -3,9 +3,18 @@ import { Order } from '../../types/Order';
 import { api } from '../../utils/api';
 import OrdersBoard from '../OrdersBoard';
 import { Container } from './styles';
+import socketIo from 'socket.io-client';
 
 const Orders = () => {
   const [orders, setOrders] = useState<Order[]>([]);
+  useEffect(() => {
+    const socket = socketIo('http://localhost:3001', {
+      transports: ['websocket'],
+    });
+    socket.on('orders@new', (order) => {
+      setOrders((prevState) => prevState.concat(order));
+    });
+  }, []);
   useEffect(() => {
     api.get('/orders').then(({ data }) => {
       setOrders(data);
